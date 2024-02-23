5e3f95688ee2774fb4467168b69f54ea7327c1dd
Joseph Rydel <79807440+CT7692@users.noreply.github.com>
2024-02-22 18:11:38 -0600

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from tkinter import messagebox
-import selenium.common.exceptions as exc
 import os
 
 def open_browser():
